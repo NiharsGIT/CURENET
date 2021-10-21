67d675ef67a2b577ce6c7ffeed07aa34bcaaa73c
Aadarsh <aadarshraj56@gmail.com>
2021-10-21 21:33:02 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Get from './Get'
+import Post from './Post'
+// import Post from './Post'
+ReactDOM.render(
+  <div>
+  <Get />,
+  <Post />
+  </div>,
+  document.getElementById('root')
+);
